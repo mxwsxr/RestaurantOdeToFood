3d02baabc4e6f050a58c22397d9ebdf5ec5c4f1b
Albert <Albert@DESKTOP-923AJ44>
2020-10-05 22:51:29 +0200

    added new scaffolded items from Razor Page (CRUD)

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -25,7 +25,7 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<OdeToFoodDbContext>(options =>
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
             });
