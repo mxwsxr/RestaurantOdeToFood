e8a2ba2640a33d7a9286432fe9ef3f7ee12c7ca7
Albert <Albert@DESKTOP-923AJ44>
2020-10-05 22:18:07 +0200

    added database for odetofood

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -18,6 +18,8 @@ namespace OdeToFood.Data
 
         Restaurant Delete(int id);
 
+        int GetCountOfRestaurants();
+
         int Commit();
     }
 }
--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -67,5 +67,10 @@ namespace OdeToFood.Data
             }
             return restaurant;
         }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
     }
 }
